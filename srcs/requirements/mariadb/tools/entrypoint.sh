013fa67a045bedaecb51fe771b479771660e8348
amhma <amir.mahla@icloud.com>
2022-11-24 20:22:38 +0100

    mariadb done

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
 
-service mysql start
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';
 
-echo "DELETE FROM mysql.user WHERE User='';" | mysql -u root
-echo "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');" | mysql -u root
-echo "DROP DATABASE IF EXISTS test;" | mysql -u root
-echo "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';" | mysql -u root
+CREATE USER IF NOT EXISTS 'amahla'@'%' IDENTIFIED BY 'Secret94';
+CREATE DATABASE IF NOT EXISTS wordpressdb;
+GRANT ALL PRIVILEGES ON wordpressdb.* TO amahla@'%';
+FLUSH PRIVILEGES;
 
-echo "CREATE USER IF NOT EXISTS 'amahla'@'%' IDENTIFIED BY 'Secret94';" | mysql -u root
-echo "CREATE DATABASE IF NOT EXISTS wordpressdb;" | mysql -u root
-echo "GRANT ALL PRIVILEGES ON wordpressdb.* TO amahla@'%';" | mysql -u root
-echo "FLUSH PRIVILEGES;" | mysql -u root
-
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'BossSecret94';" | mysql -u root
-
-service mysql stop
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'BossSecret94';
