cd5672c7b7a795e1b1b10c3e91b540c00cd0e5eb
amhma <amir.mahla@icloud.com>
2022-11-24 19:50:37 +0100

    Mariadb docker in progress : 2.0

--- srcs/requirements/mariadb/tools/entrypoint2.sh
+++ srcs/requirements/mariadb/tools/entrypoint2.sh
@@ -1,21 +1,13 @@
 #!/bin/sh
 
-service mysql start
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';
 
-mysql -u root -p 
-""
-EOF
+CREATE USER IF NOT EXISTS 'amahla'@'%' IDENTIFIED BY 'Secret94';
+CREATE DATABASE IF NOT EXISTS wordpressdb;
+GRANT ALL PRIVILEGES ON wordpressdb.* TO amahla@'%';
+FLUSH PRIVILEGES;
 
-#DELETE FROM mysql.user WHERE User='';
-#DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')
-#DROP DATABASE IF EXISTS test;
-#DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';
-
-#CREATE USER IF NOT EXISTS 'amahla'@'%' IDENTIFIED BY 'Secret94';
-#CREATE DATABASE IF NOT EXISTS wordpressdb;
-#GRANT ALL PRIVILEGES ON wordpressdb.* TO amahla@'%';
-#FLUSH PRIVILEGES;
-
-#ALTER USER 'root'@'localhost' IDENTIFIED BY 'BossSecret94';
-
-#service mysql stop
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'BossSecret94';
