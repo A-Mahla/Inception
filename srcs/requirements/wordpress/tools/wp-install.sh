e31be58891adda82e72eeb1fcb35708c9ea597f6
amhma <amir.mahla@icloud.com>
2022-11-28 19:38:11 +0100

    wordpress on

---
+++ srcs/requirements/wordpress/tools/wp-install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -f /var/www/html/wp-config.php ]
+then
+	echo "Wordpress is already downloaded, configured and install"
+else
+	wp core download --allow-root
+	wp config create --allow-root --dbhost=mariadb:3306 --dbname=wordpressdb \
+	--dbuser=amahla --dbpass=Secret94
+	wp core install --allow-root --url="amahla.42.fr" --title="Inception" \
+	--admin_user="supervisor" --admin_password="StrongSecret94" --admin_email="amahla@supervisor.com"
+fi
+echo "Wordpress is running"
+php-fpm7.3 -F
