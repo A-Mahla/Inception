2389d7e42cc1f6fa1c63d04bb85f0f7d367b7856
amhma <amir.mahla@icloud.com>
2022-11-29 16:28:05 +0100

    save

--- srcs/requirements/wordpress/tools/wp-install.sh
+++ srcs/requirements/wordpress/tools/wp-install.sh
@@ -5,10 +5,11 @@ then
 	echo "Wordpress is already downloaded, configured and installed"
 else
 	wp core download --allow-root
-	wp config create --allow-root --dbhost=mariadb:3306 --dbname=wordpressdb \
-	--dbuser=amahla --dbpass=Secret94
-	wp core install --allow-root --url="amahla.42.fr" --title="Inception" \
-	--admin_user="supervisor" --admin_password="StrongSecret94" --admin_email="amahla@supervisor.com"
+	wp config create --allow-root --dbhost=mariadb:3306 --dbname=$DB_NAME \
+	--dbuser=$DB_USER --dbpass=$DB_USER_PWD
+	wp core install --allow-root --url="$DOMAIN_NAME" --title="Inception" \
+	--admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PWD" --admin_email="$WP_ADMIN_MAIL"
+	wp user create $WP_USER $WP_USER_MAIL --user_pass=$WP_USER_PWD
 fi
 echo "Wordpress is running"
 php-fpm7.3 -F
