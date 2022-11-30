aa4747f2c1e1fd50a9d15296b67f585a446554c3
amhma <amir.mahla@icloud.com>
2022-11-30 16:55:59 +0100

    mariadb done ...

--- srcs/requirements/mariadb/tools/config.sh
+++ srcs/requirements/mariadb/tools/config.sh
@@ -1,58 +1,19 @@
-#service mysql start
+#!/bin/sh
 
-#/etc/init.d/mysql start
-
-if [ ! -d /var/lib/mysql/mysql ]
+if [ ! -f "/var/lib/mysql/.config_mysql_ok" ]
 then
-mysql_install_db > /dev/null
 
+	echo "INITIALISATION"
+	mysql_install_db > /dev/null
 
-mysqld_safe & sleep 2
+	mysqld_safe & sleep 2
 
-mysql -u root --skip-password << EOF
-DELETE FROM mysql.user WHERE User='';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-DROP DATABASE IF EXISTS test;
-DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';
-CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PWD';
-CREATE DATABASE IF NOT EXISTS $DB_NAME;
-GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@'%';
-FLUSH PRIVILEGES;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PWD';
-EOF
+	envsubst < /tmp/config.sql > /tmp/config_exp.sql
+	mysql < /tmp/config_exp.sql
 
-#sleep 2
-#ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PWD';
+	mysqladmin -uroot -p$DB_ROOT_PWD shutdown
 
-#mysql -e "DELETE FROM mysql.user WHERE User='';"
-#mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
-#mysql -e "DROP DATABASE IF EXISTS test;"
-#mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';"
-#mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PWD';"
-#mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
-#mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@'%';"
-#mysql -e "FLUSH PRIVILEGES;"
-#mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PWD';"
-
-#echo "DELETE FROM mysql.user WHERE User='';" | mysql
-#echo "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');" | mysql
-#echo "DROP DATABASE IF EXISTS test;" | mysql
-#echo "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';" | mysql
-#echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PWD';" | mysql
-#echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" | mysql
-#echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@'%';" | mysql
-#echo "FLUSH PRIVILEGES;" | mysql
-#echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PWD';" | mysql
-
-#/etc/init.d/mysql stop
-#mysql
-
-#service mysql stop
-#mysqladmin ping
-
-mysqladmin -uroot -p$DB_ROOT_PWD shutdown
-#systemctl stop mariadb
-#sleep 2
+	touch /var/lib/mysql/.config_sql_ok
 fi
 
-exec mysqld -u root
+exec mysqld
---
+++ srcs/requirements/mariadb/tools/config_test.sh
@@ -0,0 +1,58 @@
+#service mysql start
+
+#/etc/init.d/mysql start
+
+if [ ! -d /var/lib/mysql/mysql ]
+then
+mysql_install_db > /dev/null
+
+
+mysqld_safe & sleep 2
+
+mysql -u root --skip-password << EOF
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';
+CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PWD';
+CREATE DATABASE IF NOT EXISTS $DB_NAME;
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@'%';
+FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PWD';
+EOF
+
+#sleep 2
+#ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PWD';
+
+#mysql -e "DELETE FROM mysql.user WHERE User='';"
+#mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+#mysql -e "DROP DATABASE IF EXISTS test;"
+#mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';"
+#mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PWD';"
+#mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+#mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@'%';"
+#mysql -e "FLUSH PRIVILEGES;"
+#mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PWD';"
+
+#echo "DELETE FROM mysql.user WHERE User='';" | mysql
+#echo "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');" | mysql
+#echo "DROP DATABASE IF EXISTS test;" | mysql
+#echo "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';" | mysql
+#echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PWD';" | mysql
+#echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" | mysql
+#echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@'%';" | mysql
+#echo "FLUSH PRIVILEGES;" | mysql
+#echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PWD';" | mysql
+
+#/etc/init.d/mysql stop
+#mysql
+
+#service mysql stop
+#mysqladmin ping
+
+mysqladmin -uroot -p$DB_ROOT_PWD shutdown
+#systemctl stop mariadb
+#sleep 2
+fi
+
+exec mysqld -u root
